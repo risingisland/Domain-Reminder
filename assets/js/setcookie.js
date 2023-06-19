88d51d37461e22a21846a816b8f423f505f15c15
risingisland <risingisland@yahoo.com>
2023-06-19 16:18:19 +0100

    Update setcookie.js

--- assets/js/setcookie.js
+++ assets/js/setcookie.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-	var loginForm = document.getElementById("login-form");
-	loginForm.addEventListener("submit", function(event) {
-		var rememberMeCheckbox = document.getElementById("remember_me");
-		if (rememberMeCheckbox.checked) {
-			var usernameInput = document.getElementById("username");
-			var username = usernameInput.value;
-			var expirationDate = new Date();
-			expirationDate.setDate(expirationDate.getDate() + 90);
-			document.cookie = "domain_reminder=" + encodeURIComponent(username) + "; expires=" + expirationDate.toUTCString() + "; path=/";
-		}
-	});
-});
+
